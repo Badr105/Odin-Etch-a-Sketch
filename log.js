d0414cc54a59bee4f411a7b39f135c669091d760
BadrG <bguellass@gmail.com>
2025-07-31 11:22:59 +0100

    Added features. 16 by 16 grid is made. when hover over the grid the color changes.

--- log.js
+++ log.js
@@ -1,1 +1,17 @@
- 
+const container = document.querySelector(".container");
+
+
+for (let i = 0; i < 16; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
+
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.addEventListener("mouseover", function(e) {
+            square.style.backgroundColor = "blue";
+        });
+        column.appendChild(square);
+    }
+}
