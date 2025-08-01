063ee7e9255e7171819be63d7e6c22c82ef51f92
BadrG <bguellass@gmail.com>
2025-08-01 12:35:56 +0100

    Added buttons to select size of grid and reset it

--- log.js
+++ log.js
@@ -1,17 +1,34 @@
 const container = document.querySelector(".container");
 
+function createGrid() {
+    checkGrid();
+    let size;
 
-for (let i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    container.appendChild(column);
+    while (true) {
+        size = prompt("Select the size of the grid (between 16 and 100): ");
+        if (!(size > 100 || size < 16)) {
+            break;
+        }
+    }
+    
+    for (let i = 0; i < size; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        container.appendChild(column);
 
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", function(e) {
-            square.style.backgroundColor = "blue";
-        });
-        column.appendChild(square);
+        for (let i = 0; i < size; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", function(e) {
+                square.style.backgroundColor = "mediumaquamarine";
+            });
+            column.appendChild(square);
+        }
+    }
+}
+
+function checkGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
 }
